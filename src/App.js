280add8e586ed5d5581733e1f1819f930962ae1f
Würfel Wolfgang <wolfgang.wuerfel@infonova.com>
2020-07-30 15:11:55 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi there 
+          Feature change
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          UnLearn React
+          Learn React
         </a>
       </header>
     </div>
